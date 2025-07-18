164d4eeded08e328f4ffd5e8bd239a245a4d6abb
LeoM0303 <leomuliarchuk51@gmail.com>
2025-07-18 23:08:06 +0300

    add API

--- main.go
+++ main.go
@@ -5,16 +5,28 @@ import (
 	"net/http"
 )
 
-func main() {
-	fmt.Println("##### Welcome TodoList App! #####\n")
+var task1 = "Watch night movie"
+var task2 = "Read Book"
+var task3 = "Add Book"
+var task4 = "Write code"
+var taskIteams = []string{task1, task2, task3, task4}
 
+func main() {
 	http.HandleFunc("/", helloUser)
+	http.HandleFunc("/show-tasks", showTask)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func helloUser(writer http.ResponseWriter, request *http.Request) {
+func showTask(writer http.ResponseWriter, request *http.Request) {
+	for index, task := range taskIteams {
+		fmt.Fprintln(writer, index+1, task)
+	}
+}
 
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "Hello user! Welcome to our TodoList App!"
+	fmt.Fprintln(writer, greeting)
 }
 
 func printTask(taskIteams []string) {
