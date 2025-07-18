996308bc9a1acf6caea6c1dabd876948edfae8f9
LeoM0303 <leomuliarchuk51@gmail.com>
2025-07-18 22:10:23 +0300

    fix bug

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ var taskIteams = []string{task1, task2, task3, task4}
 
 func main() {
 	fmt.Println("##### Welcome TodoList App! #####\n")
+	printTask()
 }
 
 func printTask() {
-
 	fmt.Println("List of my task")
 	for index, task := range taskIteams {
 		fmt.Printf("%d: %s\n\n", index+1, task)
