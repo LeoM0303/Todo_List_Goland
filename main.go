7226b58ac4e03384964961006467285fa266ff48
LeoM0303 <leomuliarchuk51@gmail.com>
2025-07-18 22:06:58 +0300

    create new function

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import "fmt"
 
+var task1 = "Watch night movie"
+var task2 = "Read Book"
+var task3 = "Add Book"
+var task4 = "Write code"
+
+var taskIteams = []string{task1, task2, task3, task4}
+
 func main() {
-
-	var task1 = "Watch night movie"
-	var task2 = "Read Book"
-	var task3 = "Add Book"
-	var task4 = "Write code"
-
-	var taskIteams = []string{task1, task2, task3, task4}
 	fmt.Println("##### Welcome TodoList App! #####\n")
+}
 
+func printTask() {
+
+	fmt.Println("List of my task")
 	for index, task := range taskIteams {
 		fmt.Printf("%d: %s\n\n", index+1, task)
 	}
