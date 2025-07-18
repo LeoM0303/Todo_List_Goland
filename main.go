6e2475b9c24e0061be0f265ed6e27a0a6123c53b
LeoM0303 <leomuliarchuk51@gmail.com>
2025-07-18 19:31:04 +0300

    trying work

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World\n")
+	fmt.Println("##### Welcome TodoList App! #####\n")
+
+	fmt.Println("1. Read Book\n")
+	fmt.Println("2. Go to gym\n")
+	fmt.Println("3. Write code\n")
 }
