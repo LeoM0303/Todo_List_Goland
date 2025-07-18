0a247b96618381b2bfffc225b2ba63982b1ec682
LeoM0303 <leomuliarchuk51@gmail.com>
2025-07-18 22:17:25 +0300

    redoing struct

--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import "fmt"
 
-var task1 = "Watch night movie"
-var task2 = "Read Book"
-var task3 = "Add Book"
-var task4 = "Write code"
-
-var taskIteams = []string{task1, task2, task3, task4}
-
 func main() {
 	fmt.Println("##### Welcome TodoList App! #####\n")
-	printTask()
+
+	var task1 = "Watch night movie"
+	var task2 = "Read Book"
+	var task3 = "Add Book"
+	var task4 = "Write code"
+
+	var taskIteams = []string{task1, task2, task3, task4}
+
+	printTask(taskIteams)
 }
 
-func printTask() {
+func printTask(taskIteams []string) {
 	fmt.Println("List of my task")
 	for index, task := range taskIteams {
 		fmt.Printf("%d: %s\n\n", index+1, task)
