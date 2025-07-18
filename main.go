8ec60552b510d227adb08f16e961c017315b529c
LeoM0303 <leomuliarchuk51@gmail.com>
2025-07-18 19:38:31 +0300

    create first "var"

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
+
+	var task1 = "Watch night movie"
+
 	fmt.Println("##### Welcome TodoList App! #####\n")
 
 	fmt.Println("1. Read Book\n")
 	fmt.Println("2. Go to gym\n")
 	fmt.Println("3. Write code\n")
+	fmt.Println("4.", task1)
 }
